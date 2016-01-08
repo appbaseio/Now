53b3679772907be007dcde2e7fa54d473ac9f731
Siddharth Kothari <sids.aquarius@gmail.com>
2016-01-08 14:20:06 +0530

    update js files, lint

--- src/assets/js/dataController.js
+++ src/assets/js/dataController.js
@@ -1,78 +1,69 @@
-var app =  angular.module('myApp');
-app.service('dataClient',function(client){
+var app = angular.module('myApp');
 
-    var streamingClient = new Appbase({
-       url: 'https://scalr.api.appbase.io',
-       appname: 'checkin',
-       username: '6PdfXag4h',
-       password: 'b614d8fa-03d8-4005-b6f1-f2ff31cd0f91'
-     });
+app.service('dataClient', function() {
 
-    this.getSuggestions = function(text){
+  var appbaseRef = new Appbase({
+    url: 'https://scalr.api.appbase.io',
+    appname: 'checkin',
+    username: '6PdfXag4h',
+    password: 'b614d8fa-03d8-4005-b6f1-f2ff31cd0f91'
+  });
 
-    return streamingClient.search({
-          index: 'checkin',
-          body: {
-            suggest: {
-              mysuggester: {
-                text: text,
-                completion: {
-                  field: 'city_suggest'
-                }
-              }
-            }
-          }
-      });
-    }
-
-    this.getSearchCheckin = function(text){
-      return streamingClient.search({
-        index: 'checkin',
-        type: 'city',
-        body: {
-          query : {
-            match: {
-              city : text
+  this.getSuggestions = function(text) {
+    return appbaseRef.search({
+      index: 'checkin',
+      body: {
+        suggest: {
+          mysuggester: {
+            text: text,
+            completion: {
+              field: 'city_suggest'
             }
           }
         }
-      });
-    }
-
-    this.getliveData = function(){
-      return streamingClient.searchStream({
-        type: 'city',
-        size: 200,
-        body: {
-          query: {
-            match_all: {}
-          }
-        },
-        streamonly: true
-      });
-    }
-
-    this.getDragData = function(lat,lng){
-      return streamingClient.search({
-        type: 'city',
-        body: {
-          query : {
-            match_all : {}
-          },
-          filter : {
-            geo_distance : {
-              distance : "2000km",
-              location : [lat,lng]
-            }
-          }
-        }
-      });
-    }
-
-});
-
-myApp.service('client', function (esFactory) {
-    return esFactory({
-        host: 'https://6PdfXag4h:b614d8fa-03d8-4005-b6f1-f2ff31cd0f91@scalr.api.appbase.io',
+      }
     });
+  }
+
+  this.getSearchCheckin = function(text) {
+    return appbaseRef.search({
+      index: 'checkin',
+      type: 'city',
+      body: {
+        query: {
+          match: {
+            city: text
+          }
+        }
+      }
+    });
+  }
+
+  this.getliveData = function() {
+    return appbaseRef.searchStream({
+      type: 'city',
+      body: {
+        query: {
+          match_all: {}
+        }
+      }
+    });
+  }
+
+  this.getDragData = function(lat, lng) {
+    return appbaseRef.search({
+      type: 'city',
+      body: {
+        query: {
+          filter: {
+            geo_distance: {
+              distance: "2000km",
+              location: [lat, lng]
+            }
+          }
+        }
+      }
+    });
+  }
+
 });
--- src/assets/js/helperFactory.js
+++ src/assets/js/helperFactory.js
@@ -1,95 +1,93 @@
-var app =  angular.module('myApp');
-app.factory('helper',function(){
+var app = angular.module('myApp');
+app.factory('helper', function() {
   return {
-    getCenter:getCenter,
-    getMapArray:getMapArray,
-    getStatus:getStatus
+    getCenter: getCenter,
+    getMapArray: getMapArray,
+    getStatus: getStatus
   };
 
-  function getCenter(zoomIndex, data){
+  function getCenter(zoomIndex, data) {
     var center;
-    switch(zoomIndex){
-        case 2:
-               center = new google.maps.LatLng(data[1]+9.6,data[2]-2.3);
-               break;
-        case 3:
-               center = new google.maps.LatLng(data[1]+4.8,data[2]-1.7);
-               break;
-        case 4:
-               center = new google.maps.LatLng(data[1]+2.4,data[2]-1.1);
-               break;
-        case 5:
-               center = new google.maps.LatLng(data[1]+1.2,data[2]-0.5);
-               break;
-        case 6:
-               center = new google.maps.LatLng(data[1]+0.6,data[2]-0.14);
-               break;
-        case 7:
-               center = new google.maps.LatLng(data[1]+0.3,data[2]-0.09);
-               break;
-        case 8:
-               center = new google.maps.LatLng(data[1]+0.15,data[2]-0.036);
-               break;
-        case 9:
-               center = new google.maps.LatLng(data[1]+0.07,data[2]-0.02);
-               break;
-        case 10:
-               center = new google.maps.LatLng(data[1]+0.04,data[2]-0.009);
-               break;
-        case 11:
-               center = new google.maps.LatLng(data[1]+0.02,data[2]-0.005);
-               break;
-        case 12:
-               center = new google.maps.LatLng(data[1]+0.01,data[2]-0.004);
-               break;
-        case 13:
-               center = new google.maps.LatLng(data[1]+0.005,data[2]-0.0009);
-               break;
-        case 14:
-               center = new google.maps.LatLng(data[1]+0.0025,data[2]-0.0005);
-               break;
-        case 15:
-               center = new google.maps.LatLng(data[1]+0.0012,data[2]-0.00027);
-               break;
-      }
+    switch (zoomIndex) {
+      case 2:
+        center = new google.maps.LatLng(data[1] + 9.6, data[2] - 2.3);
+        break;
+      case 3:
+        center = new google.maps.LatLng(data[1] + 4.8, data[2] - 1.7);
+        break;
+      case 4:
+        center = new google.maps.LatLng(data[1] + 2.4, data[2] - 1.1);
+        break;
+      case 5:
+        center = new google.maps.LatLng(data[1] + 1.2, data[2] - 0.5);
+        break;
+      case 6:
+        center = new google.maps.LatLng(data[1] + 0.6, data[2] - 0.14);
+        break;
+      case 7:
+        center = new google.maps.LatLng(data[1] + 0.3, data[2] - 0.09);
+        break;
+      case 8:
+        center = new google.maps.LatLng(data[1] + 0.15, data[2] - 0.036);
+        break;
+      case 9:
+        center = new google.maps.LatLng(data[1] + 0.07, data[2] - 0.02);
+        break;
+      case 10:
+        center = new google.maps.LatLng(data[1] + 0.04, data[2] - 0.009);
+        break;
+      case 11:
+        center = new google.maps.LatLng(data[1] + 0.02, data[2] - 0.005);
+        break;
+      case 12:
+        center = new google.maps.LatLng(data[1] + 0.01, data[2] - 0.004);
+        break;
+      case 13:
+        center = new google.maps.LatLng(data[1] + 0.005, data[2] - 0.0009);
+        break;
+      case 14:
+        center = new google.maps.LatLng(data[1] + 0.0025, data[2] - 0.0005);
+        break;
+      case 15:
+        center = new google.maps.LatLng(data[1] + 0.0012, data[2] - 0.00027);
+        break;
+    }
     return center;
   }
 
-  function getMapArray(current_hit){
-    console.log(current_hit);
-    var arr =
-      [
-        current_hit.shout,
-        current_hit.latitude,
-        current_hit.longitude,
-        1,
-        current_hit.category,
-        current_hit.url,
-        current_hit.photourl,
-        current_hit.venue,
-        current_hit.username
-      ];
-      return arr;
+  function getMapArray(current_hit) {
+    var arr = [
+      current_hit.shout,
+      current_hit.latitude,
+      current_hit.longitude,
+      1,
+      current_hit.category,
+      current_hit.url,
+      current_hit.photourl,
+      current_hit.venue,
+      current_hit.username
+    ];
+    return arr;
   }
 
-  function getStatus(checkinLength){
+  function getStatus(checkinLength) {
     var obj = {};
-    if(checkinLength<1){
-        obj.checkinfrequency=checkinLength;
-        obj.color = 'rgb(255, 92, 92)';
-        obj.freqtext = "waiting for new checkin streams.";
-        obj.freqclass = '';
-   }else if(checkinLength>=1 && checkinLength<2){
-        obj.checkinfrequency=checkinLength;
-        obj.color = 'rgb(255, 153, 51)';
-        obj.freqtext = "active and streaming new checkins";
-        obj.freqclass = 'active';
-   }else if(checkinLength>=2){
-        obj.checkinfrequency=checkinLength;
-        obj.color = 'rgb(51, 204, 51)';
-        obj.freqtext = "active and streaming new checkins";
-        obj.freqclass = 'active';
-   }
-   return obj;
+    if (checkinLength < 1) {
+      obj.checkinfrequency = checkinLength;
+      obj.color = 'rgb(255, 92, 92)';
+      obj.freqtext = "waiting for new checkin streams.";
+      obj.freqclass = '';
+    } else if (checkinLength >= 1 && checkinLength < 2) {
+      obj.checkinfrequency = checkinLength;
+      obj.color = 'rgb(255, 153, 51)';
+      obj.freqtext = "active and streaming new checkins";
+      obj.freqclass = 'active';
+    } else if (checkinLength >= 2) {
+      obj.checkinfrequency = checkinLength;
+      obj.color = 'rgb(51, 204, 51)';
+      obj.freqtext = "active and streaming new checkins";
+      obj.freqclass = 'active';
+    }
+    return obj;
   }
 });
